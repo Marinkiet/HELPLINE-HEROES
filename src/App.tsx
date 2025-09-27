2df2a22d0d558f8711e37c5692383e18896019b6
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2025-09-27 09:41:05 +0200

    Fix useEffect import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { useAudio } from './contexts/AudioContext';
 import { AudioProvider } from './contexts/AudioContext';
 import { EngagementProvider } from './contexts/EngagementContext';
